31708ecc5dcad23f2b6ce3322369538c46488239
aziza.kalmasova <aziza.kalmasova@phystech.edu>
2020-05-25 13:08:05 +0300

    No csv, only sheet names

--- src/main/java/org/apache/poi/xssf/eventusermodel/XLSX2CSV.java
+++ src/main/java/org/apache/poi/xssf/eventusermodel/XLSX2CSV.java
@@ -1,4 +1,5 @@
-package org.apache.poi.xssf.eventusermodel;/* ====================================================================
+package org.apache.poi.xssf.eventusermodel;
+/* ====================================================================
    Licensed to the Apache Software Foundation (ASF) under one or more
    contributor license agreements.  See the NOTICE file distributed with
    this work for additional information regarding copyright ownership.
